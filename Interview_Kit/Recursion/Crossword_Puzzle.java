566b60c97b4e23a58adaf46869dd1eab1d647682
dbsxodud11 <99yty@kaist.ac.kr>
2020-09-16 22:03:44 +0900

    crossword puzzle - finally solved..

--- Interview_Kit/Recursion/Crossword_Puzzle.java
+++ Interview_Kit/Recursion/Crossword_Puzzle.java
@@ -6,81 +6,96 @@ import java.util.*;
 public class Crossword_Puzzle {
 
     // Complete the crosswordPuzzle function below.
+    public static String[][] answer;
     static String[] crosswordPuzzle(String[] crossword, String words) {
 
         //Prepare a word list
         String[] word_list = words.split(";");
-        boolean[] is_used = new boolean[word_list.length];
-        char[][] cross_word = new char[crossword.length][crossword[0].length()];
+        String[][] cross_word = new String[crossword.length][crossword[0].length()];
         for(int i=0; i<crossword.length; i++){
             for(int j=0; j<crossword[0].length(); j++){
-                cross_word[i][j] = crossword[i].charAt(j);
+                cross_word[i][j] = Character.toString(crossword[i].charAt(j));
             }
         }
-        cross_word = solvePuzzle(cross_word, word_list, is_used);
-        String[] answer = new String[cross_word.length];
+        solvePuzzle(cross_word, word_list, 0);
+        String[] result = new String[answer.length];
         for(int i=0; i<cross_word.length; i++){
             StringBuilder sb = new StringBuilder();
-            for(int j=0; j<cross_word[0].length; j++){
-                sb.append(cross_word[i][j]);
+            for(int j=0; j<answer[0].length; j++){
+                sb.append(answer[i][j]);
             }
-            answer[i] = sb.toString();
+            result[i] = sb.toString();
         }
-        return answer;
+        return result;
     }
 
-    private static char[][] solvePuzzle(char[][] crossword, String[] word_list, boolean[] is_used){
-        for(int i=0; i<crossword.length; i++){
-            for(int j=0; j<crossword[0].length; j++){
-                if(crossword[i][j] != '-') continue;
-                else{
-                    if(i != crossword.length-1 && crossword[i+1][j] == '-'){
-                        //Vertical
-                        int wordLength = 0;
-                        while(i != crossword.length && crossword[i][j] == '-'){
-                            wordLength++;
-                            i++;
-                        }
-                        for(int l=0; l<word_list.length; l++){
-                            if(is_used[l]) continue;
-                            String word = word_list[l];
-                            if(wordLength == word.length()){
-                                for(int k=0; k<wordLength; k++){
-                                    if(j != 0 && crossword[i-wordLength+k][j-1] == '-') continue;
-                                    else if(j != crossword[0].length-1 && crossword[i-wordLength+k][j+1] == '-') continue;
-                                    crossword[i-wordLength+k][j] = word.charAt(k);
-                                }
-                                is_used[l] = true;
-                                break;
-                            }
-                        }
-                        return solvePuzzle(crossword, word_list, is_used);
-                    }
-                    if(j != crossword[0].length-1 && crossword[i][j+1] == '-'){
-                        int wordLength = 0;
-                        while(j != crossword[0].length && crossword[i][j] == '-'){
-                            wordLength++;
-                            j++;
-                        }
-                        for(int l=0; l<word_list.length; l++){
-                            if(is_used[l]) continue;
-                            String word = word_list[l];
-                            if(wordLength == word.length()){
-                                for(int k=0; k<wordLength; k++){
-                                    if(i != 0 && crossword[i-1][j-wordLength+k] == '-') continue;
-                                    else if(i != crossword[0].length-1 && crossword[i+1][j-wordLength+k] == '-') continue;
-                                    crossword[i][j-wordLength+k] = word.charAt(k);
-                                }
-                                is_used[l] = true;
-                                break;
-                            }
-                        }
-                        return solvePuzzle(crossword, word_list, is_used);
+
+    private static void solvePuzzle(String[][] crossword, String[] word_list, int index){
+        if(answer != null) return;
+        if(index < word_list.length){
+            String word = word_list[index];
+            int maxStart = crossword.length - word.length();
+            String[][] temp = new String[10][10];
+            //1. Horizontal
+            for(int i=0; i<crossword.length; i++){
+                for(int j=0; j<=maxStart; j++){
+                    temp = checkHorizontal(i, j, crossword, word);
+                    if(!temp[0][0].equals("x")){
+                        solvePuzzle(temp, word_list, index+1);
                     }
                 }
             }
+            //2. Vertical
+            for(int j=0; j<crossword.length; j++){
+                for(int i=0; i<=maxStart; i++){
+                    temp = checkVertical(i, j, crossword, word);
+                    if(!temp[0][0].equals("x")){
+                        solvePuzzle(temp, word_list, index+1);
+                    }
+                }
+            }
+        }else{
+            answer = crossword;
+            return;
         }
-        return crossword;
+    }
+
+    private static String[][] checkHorizontal(int i, int j, String[][] crossword, String word){
+        String[][] temp = new String[crossword.length][crossword[0].length];
+        for(int a=0; a<crossword.length; a++){
+            for(int b=0; b<crossword.length; b++){
+                temp[a][b] = crossword[a][b];
+            }
+        }
+        for(int index=0; index<word.length(); index++){
+            //System.out.println(crossword[i][j+index] + " " + word.charAt(index));
+            if(crossword[i][j+index].equals("-") || crossword[i][j+index].equals(Character.toString(word.charAt(index)))){
+                temp[i][j+index] = Character.toString(word.charAt(index));
+            }else{
+                temp[0][0] = "x";
+                return temp;
+            }
+        }
+        return temp;
+    }
+
+    private static String[][] checkVertical(int i, int j, String[][] crossword, String word){
+        String[][] temp = new String[crossword.length][crossword[0].length];
+        for(int a=0; a<crossword.length; a++){
+            for(int b=0; b<crossword.length; b++){
+                temp[a][b] = crossword[a][b];
+            }
+        }
+        for(int index=0; index<word.length(); index++){
+            //System.out.println(crossword[i][j+index] + " " + word.charAt(index));
+            if(crossword[i+index][j].equals("-") || crossword[i+index][j].equals(Character.toString(word.charAt(index)))){
+               temp[i+index][j] = Character.toString(word.charAt(index));
+            }else{
+                temp[0][0] = "x";
+                return temp;
+            }
+        }
+        return temp;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
