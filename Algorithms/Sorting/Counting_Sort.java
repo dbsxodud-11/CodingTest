9d06ee273b37a48eaf5ee07218c431602ed23c17
dbsxodud11 <99yty@kaist.ac.kr>
2020-10-28 20:57:37 +0900

    Counting Sort - using StringBuilder

--- Algorithms/Sorting/Counting_Sort.java
+++ Algorithms/Sorting/Counting_Sort.java
@@ -10,44 +10,76 @@ public class Counting_Sort {
     // Complete the countSort function below.
     static void countSort(List<List<String>> arr) {
 
-        //1. Count occurence
-        int[] ranking = new int[arr.size()];
-        String[] words = new String[arr.size()];
-        int index = 0;
-        Map<Integer, Integer> treemap = new TreeMap<>();
-        for(List<String> list : arr){
-            int rank = Integer.parseInt(list.get(0));
-            ranking[index] = rank;
-            words[index] = list.get(1);
-            treemap.put(rank, treemap.getOrDefault(rank, 0)+1);
-            index++;
-        }
-        int before = 0;
-        for(int rank : treemap.keySet()){
-            treemap.put(rank, treemap.get(rank) + before);
-            before = treemap.get(rank);
-        }
-
-        // for(int rank : treemap.keySet()){
-        //     System.out.println(treemap.get(rank));
+        // //1. Count occurence
+        // int[] ranking = new int[arr.size()];
+        // String[] words = new String[arr.size()];
+        // int index = 0;
+        // int max = 0;
+        // Map<Integer, Integer> treemap = new HashMap<>();
+        // for(List<String> list : arr){
+        //     int rank = Integer.parseInt(list.get(0));
+        //     max = (rank > max) ? rank : max;
+        //     ranking[index] = rank;
+        //     words[index] = list.get(1);
+        //     treemap.put(rank, treemap.getOrDefault(rank, 0)+1);
+        //     index++;
+        // }
+        // int before = 0;
+        // for(int i=0; i<=max; i++){
+        //     if(treemap.containsKey(i)){
+        //         treemap.put(i, treemap.get(i) + before);
+        //         before = treemap.get(i);
+        //     }
         // }
 
-        //2. Counting Sort
-        String[] result = new String[arr.size()];
+        // // for(int rank : treemap.keySet()){
+        // //     System.out.println(treemap.get(rank));
+        // // }
+
+        // //2. Counting Sort
+        // String[] result = new String[arr.size()];
+        // int half = arr.size()/2;
+        // for(int i=arr.size()-1; i>=0; i--){
+        //     int position = treemap.get(ranking[i]);
+        //     treemap.put(ranking[i], position-1);
+        //     if(i < half){
+        //         result[position-1] = "-";
+        //     }else{
+        //         result[position-1] = words[i];
+        //     }
+        // }
+        // StringBuilder sb = new StringBuilder();
+        // for(String element : result){
+        //     sb.append(element);
+        //     sb.append("-");
+        // }
+        // System.out.print(sb.toString());
+
+        //Second Method : Use StringBuilder Intensively
+        Map<Integer, List<StringBuilder>> treemap = new TreeMap<>();
+        int index = 0;
         int half = arr.size()/2;
-        for(int i=arr.size()-1; i>=0; i--){
-            int position = treemap.get(ranking[i]);
-            treemap.put(ranking[i], position-1);
-            if(i < half){
-                result[position-1] = "-";
+        for(List<String> list : arr){
+            int rank = Integer.parseInt(list.get(0));
+            StringBuilder sb = new StringBuilder(list.get(1));
+            if (index < half) sb = new StringBuilder("-");
+            if(treemap.containsKey(rank)){
+                treemap.get(rank).add(sb);
             }else{
-                result[position-1] = words[i];
+                List<StringBuilder> rank_list = new LinkedList<>();
+                rank_list.add(sb);
+                treemap.put(rank, rank_list);
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for(int key : treemap.keySet()){
+            for(StringBuilder word : treemap.get(key)) {
+                result.append(word);
+                result.append(" ");
             }
         }
 
-        for(String element : result){
-            System.out.print(element + " ");
-        }
+        System.out.print(result.toString());
     }
 
     public static void main(String[] args) throws IOException {
